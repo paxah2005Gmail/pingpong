18fd3834313d74f0022b0287256b3be533350ec2
paxah2005Gmail <132819955+paxah2005Gmail@users.noreply.github.com>
2023-05-14 15:38:17 +0300

    Add files via upload

--- pingpong.py
+++ pingpong.py
@@ -3,10 +3,10 @@ from random import randint
 from pygame import *
 from time import time as tm
 w_width = 1280
-w_height = 960
+w_height = 800
 window = display.set_mode((w_width, w_height))
 display.set_caption('Пинг-Понг')
-bgColor = (247,242,26)
+bgColor = (255,255,255)
 clock = time.Clock()
 font.init()
 
@@ -33,78 +33,55 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def moveLP(self):
         keys = key.get_pressed()
-        if keys[K_W] and (self.rect.y <= (0 + self.speed)):
+        if keys[K_w] and (self.rect.y >= (0 + self.speed)):
             self.rect.y -= self.speed
-        if keys[K_S] and (self.rect.y <= (w_height - self.speed - self.rect.height)):
+        if keys[K_s] and (self.rect.y <= (w_height - self.speed - self.rect.height)):
             self.rect.y += self.speed
     def moveRP(self):
         keys = key.get_pressed()
-        if keys[K_UP] and (self.rect.y <= (0 + self.speed)):
+        if keys[K_UP] and (self.rect.y >= (0 + self.speed)):
             self.rect.y -= self.speed
         if keys[K_DOWN] and (self.rect.y <= (w_height - self.speed - self.rect.height)):
             self.rect.y += self.speed
+
+class Ball(GameSprite):
+    def __init__(self, imageName, x, y, speed, sizeX=75, sizeY=75):
+        super().__init__(imageName, x, y, speed, sizeX, sizeY)
+        self.speedX = speed
+        self.speedY = speed
+    def move(self):
+        self.rect.x += self.speedX
+        self.rect.y += self.speedY
+        if (self.rect.y <= 0) or ((self.rect.y >= (w_height - self.rect.height))):
+            self.speedY *= -1
+            return ""
+        if (self.rect.x <= 0):
+            return "left"
+        if ((self.rect.x >= (w_width - self.rect.width))):
+            return "right"
 plH = 100
 plW = 25
 plL = Player('RedPlayer.png', 0, (w_height - plH)//2, 10, plW, plH)
 plR = Player('BluePlayer.png', (w_width - plW), (w_height - plH)//2, 10, plW, plH)
-#enemies = sprite.Group()
-enemies = list()
-for i in range(1):
-    enemy = Enemy('ufo.png', randint(0, 625), 0, randint(1, 4))
-    #enemies.add(enemy)
-    enemies.append(enemy)
-
+ball = Ball('ball.png', plW, (w_height - 35)//2, 7, 35, 35)
 run = True
 finish = False
-bullets = list()
-shotTime = 0
-btwnShtTm = 0.15
-score = 0
+result = ""
 while run:
     for e in event.get():
         if e.type == QUIT:
-            run = False
-        
-    window.blit(background, (0, 0))
-    if not finish:
-        pl.move()
-        if tm()-shotTime >= btwnShtTm:
-            keys = key.get_pressed()
-            if keys[K_UP]:
-                fireSound.play()
-                bullets.append(Bullet('bullet.png',
-                        pl.rect.centerx-10, pl.rect.top, 5, 20, 40))        
-                shotTime = tm()
-        for bullet in bullets:
-            bullet.update()
-            bullet.reset()
-            for enemy in enemies:
-                if sprite.collide_rect(bullet, enemy):
-                    enemy.respawn()
-                    if (score % 10) == 0:
-                        enemies.append(Enemy('ufo.png', randint(0, 625), 0, randint(1, 4 + (score // 50))))                    
-                    if bullet in bullets:
-                        bullets.remove(bullet)
-                    score += 1
-                    if score >= 20:
-                        #finish = True
-                        finish = False
-                    break
-        pl.reset()
-        #enemies.update()
-        #enemies.draw()
-        for enemy in enemies:
-            enemy.update()
-            if sprite.collide_rect(pl, enemy):
-                #finish = True
-                finish = False
-            enemy.reset()
-    else:
-        if score >= 20:
-            window.blit(winLabel, (200, 200))
-        else:
-            window.blit(loseLabel, (180, 200))
-    window.blit(font1.render('Щот: ' + str(score), True, (255,255,255)), (0, 0))
-    window.blit(font1.render('Прапусчина: ' + str(score_lose), True, (255,255,255)), (0, 30))    
+            run = False     
+    window.fill(bgColor)
+    if result == "":
+        result = ball.move()
+        plL.moveLP()
+        plR.moveRP()   
+        ball.reset()    
+        plL.reset()
+        plR.reset()
+    elif result == "left":
+        window.blit(rightLabel, ((w_width - 300)//2, (w_height - 100)//2))
+    elif result == "right":
+        window.blit(leftLabel, ((w_width - 300)//2, (w_height - 100)//2))
     display.update()
     clock.tick(60)
